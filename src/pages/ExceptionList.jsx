eecade9304f24f47c0a606561e4408dfe0dbfd42
Bhavani udagi <bhavaniudagi7@gmail.com>
2026-04-30 15:27:10 +0530

    Initial commit

---
+++ src/pages/ExceptionList.jsx
@@ -0,0 +1,11 @@
+function ExceptionList() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl text-green-600">
+        Policy Exception List Page
+      </h1>
+    </div>
+  );
+}
+
+export default ExceptionList;
